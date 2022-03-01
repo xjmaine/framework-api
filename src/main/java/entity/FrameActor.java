c60f89109b960bc38473d8c48e40ea60dd64df4c
xjmaine <xjfreddie@gmail.com>
2022-03-01 23:06:19 +0000

    POM has starter project

---
+++ src/main/java/entity/FrameActor.java
@@ -0,0 +1,49 @@
+package entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@Entity
+@Table(name= "frameactors")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"created_at", "updated_at"}, allowGetters = true)
+@Data
+public class FrameActor {
+	@Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="quote")
+	private float quote;
+	
+	@Column(nullable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date_created;
+	
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date_modifed;
+	
+	
+
+}
--- src/main/java/frameworkapi/main/FrameworkApiApplication.java
+++ src/main/java/frameworkapi/main/FrameworkApiApplication.java
@@ -2,8 +2,10 @@ package frameworkapi.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FrameworkApiApplication {
 
 	public static void main(String[] args) {
